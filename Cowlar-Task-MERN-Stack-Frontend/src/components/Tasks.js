30df0d17468ebd7cbaf7b6a1a1c7e21f9af66cf2
AliHaider18 <alixhaider7@gmail.com>
2023-03-23 17:52:46 +0500

    React Spinner Added

--- Cowlar-Task-MERN-Stack-Frontend/src/components/Tasks.js
+++ Cowlar-Task-MERN-Stack-Frontend/src/components/Tasks.js
@@ -27,6 +27,9 @@ import { HiCheckCircle } from "react-icons/hi";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+//React Spinner
+import { ColorRing } from "react-loader-spinner";
+
 //Importing from directory
 import "./Tasks.css";
 import bitmapImg from "../img/Bitmap.jpg";
@@ -296,7 +299,23 @@ const Tasks = () => {
                   </React.Fragment>
                 ))
               ) : (
-                <></>
+                <div style={{ textAlign: "center" }}>
+                  <ColorRing
+                    visible={true}
+                    height="100"
+                    width="100"
+                    ariaLabel="blocks-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="blocks-wrapper"
+                    colors={[
+                      "#e15b64",
+                      "#f47e60",
+                      "#f8b26a",
+                      "#abbd81",
+                      "#849b87",
+                    ]}
+                  />
+                </div>
               )}
             </List>
           ) : (
